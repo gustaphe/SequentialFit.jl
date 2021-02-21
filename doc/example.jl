7eca24286bdecca3a144ad1087cde052e0eb093b
David Gustavsson <david.e.gustavsson@gmail.com>
2021-02-21 15:45:44 +0100

    Improved example

--- doc/example.jl
+++ doc/example.jl
@@ -2,7 +2,7 @@
 using SequentialFit, Plots
 N = 20 # Number of frames
 
-gaussian(x,A,mu,sigma) = A*exp(-((x-mu)/sigma)^2)
+gaussian(x,A,mu,sigma) = A*exp(-sign(sigma)*((x-mu)/sigma)^2)
 p_true = [0.7,-1.5,0.4,0.9,1.2,0.7]
 
 function slowFunction(x)
@@ -16,17 +16,19 @@ end
 
 @. model(x,p) = gaussian(x,p[1:3]...) + gaussian(x,p[4:6]...)
 p_0 = [1.0,-1.0,0.5,1.0,1.0,0.5] # Initial guess of parameters. Has to be decent
-x_0 = [-2.0, -1.0, 0.0, 1.0] # Initial sample points
+#x_0 = [-2.0, 1.0] # Initial sample points
 
-s = SeqFit(slowFunction, model, x_0, p_0)
+s = SeqFit(slowFunction, model, p_0)
 err = zeros(N,length(p_0))
 
 anim = @animate for i in 1:N
     update!(s)
     err[i,:] .= abs.(s.fit.param ./ p_true .- 1)
     plot(
-         s,groundtruth=true,
-         xlim=(-5,5),ylim=(0,1),
+         s,
+         groundtruth=true,
+         selection=true,
+         xlim=(-5,5),ylim=(-0.1,1.0),
         )
 end
 
@@ -38,6 +40,7 @@ savefig(
              legend=false,
              xlabel="Iteration",
              ylabel="Error",
+             yscale=:log10,
             ),
         "doc/example_errors.png",
        )
--- src/SequentialFit.jl
+++ src/SequentialFit.jl
@@ -16,7 +16,9 @@ function SeqFit(f,model,X,p0)
     Y = f.(X)
     SeqFit(f,model,copy(X),Y,curve_fit(model,X,Y,p0))
 end
-SeqFit(f,model,p0) = SeqFit(f,model,Float64[],p0)
+function SeqFit(f,model,p0)
+    SeqFit(f,model,[-1.0,0.0,1.0],p0)
+end
 
 function push!(s::SeqFit,x...)
     push!(s.X,x...)
@@ -42,13 +44,16 @@ function selectionFunction(x,s)
 end
 
 @recipe function f(s::SeqFit; groundtruth=false, selection=false)
+
     @series begin # measurements
+        subplot:=1
         seriestype:=:scatter
         label-->"Samples"
         s.X, s.Y
     end
 
     @series begin # fit
+        subplot:=1
         seriestype:=:line
         label-->"Fit"
         x->s.model(x,s.fit.param)
@@ -56,6 +61,7 @@ end
 
     if groundtruth
         @series begin # Ground truth
+            subplot:=1
             seriestype:=:line
             linestyle-->:dash
             label-->"Ground truth"
@@ -64,10 +70,16 @@ end
     end
 
     if selection
+        layout := (2,1)
+        link := :x
         @series begin # Selection function
+            subplot:=2
             seriestype:=:line
             linestyle-->:dash
             label-->"Selection function"
+            ylims:=:auto
+            yticks:=false
+            legend:=:bottomright
             x->selectionFunction(x,s)
         end
     end
